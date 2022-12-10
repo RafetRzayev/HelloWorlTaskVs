46215b55d340c145de0a3db52b5a1531dc1e4dd7
Rafet <rafetr@risk.az>
2022-12-10 10:30:34 +0400

    Add project files.

---
+++ HelloWorlTaskVs/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorlTaskVs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("A ededini daxil et:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("B ededini daxil et:");
+            b = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"{a}%{b}={a%b}");
+        }
+    }
+}
